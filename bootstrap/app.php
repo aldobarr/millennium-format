207c19833af72bc2632f102b1db25b29ced06f58
Aldo <hadesflames@live.com>
2025-06-29 18:19:41 -0400

    Remove header requirement.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 		health: '/up',
 	)
 	->withMiddleware(function (Middleware $middleware): void {
-		$middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR);
+		$middleware->trustProxies(at: '*');
 		$middleware->prepend(ThrottleRequests::using('global'))->throttleApi();
 	})
 	->withExceptions(function (Exceptions $exceptions): void {
