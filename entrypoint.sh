bd0082fc6e7041669550d7cdb6379da20915f82d
Aldo <hadesflames@live.com>
2025-07-03 08:01:14 -0400

    Update Docker build to use current node & latest npm. Add storage:link to entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 composer install --optimize-autoloader --no-dev
 composer require laravel/octane --update-no-dev --optimize-autoloader
+php artisan storage:link
 php artisan optimize
 php artisan migrate --isolated --force
 npm install
