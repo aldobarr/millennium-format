b1fde2befb448f89b718d7360fffc1bae2439ee2
Aldo <hadesflames@live.com>
2025-07-08 22:40:33 -0400

    Fix select drop down not appearing over other elements. Add spell/trap property search to format page. Add search options to deck builder.

---
+++ resources/js/enums/Property.ts
@@ -0,0 +1,11 @@
+enum Property {
+	NORMAL = 'Normal',
+	CONTINUOUS = 'Continuous',
+	RITUAL = 'Ritual',
+	QUICK_PLAY = 'Quick-Play',
+	FIELD = 'Field',
+	EQUIP = 'Equip',
+	COUNTER = 'Counter',
+}
+
+export default Property;
