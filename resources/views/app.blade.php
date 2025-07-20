e42f67e990f3cdac347a3a952359daa55f63be8c
Aldo <hadesflames@live.com>
2025-07-20 16:54:31 -0400

    Project name change.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>{{ config('app.name', 'Project Lost') }}</title>
+		<title>{{ config('app.name', 'Millennium Format') }}</title>
 
 		<link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
