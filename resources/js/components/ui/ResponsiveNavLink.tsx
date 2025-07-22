37a6c0c89b0424cf44a6c9603cdfaa72a9b2f719
Aldo Barreras <hadesflames@live.com>
2025-07-22 00:05:52 -0400

    Responsive nav menu fixes (#106)
    Fix responsive nav items.
    Finish custom links in responsive nav.

--- resources/js/components/ui/ResponsiveNavLink.tsx
+++ resources/js/components/ui/ResponsiveNavLink.tsx
@@ -1,29 +1,30 @@
 import { Link } from '@kobalte/core/link';
-import { Component, JSX } from 'solid-js';
+import { Component, JSX, Show } from 'solid-js';
 
 const ResponsiveNavLink: Component<{
 	children?: JSX.Element;
+	class?: string;
+	classList?: { [key: string]: boolean };
 	href: string;
 	show?: boolean;
 	active?: boolean;
 	as?: keyof JSX.HTMLElementTags;
-}> = ({ as = 'a', href, show = true, active = false, children }) => {
-	if (!show) {
-		return <></>;
-	}
-
+}> = (props) => {
 	return (
-		<Link
-			as={as}
-			href={href}
-			class={`w-full flex items-start pl-3 pr-4 py-2 border-l-4 ${
-				active
-					? 'border-blue-400 text-gray-100 bg-blue-900 focus:outline-none focus:text-gray-200 focus:bg-blue-800 focus:border-blue-700'
-					: 'border-transparent text-white hover:text-gray-100 hover:bg-blue-900 hover:border-blue-700'
-			} text-base font-medium focus:outline-none transition duration-150 ease-in-out`}
-		>
-			{children}
-		</Link>
+		<Show when={props.show ?? true}>
+			<Link
+				as={props.as ?? 'a'}
+				href={props.href}
+				class={`w-full flex items-start pl-3 pr-4 py-2 border-l-4 ${
+					(props.active ?? false)
+						? 'border-blue-400 text-gray-100 bg-blue-900 focus:outline-none focus:text-gray-200 focus:bg-blue-800 focus:border-blue-700'
+						: 'border-transparent text-white hover:text-gray-100 hover:bg-blue-900 hover:border-blue-700'
+				} text-base font-medium focus:outline-none transition duration-150 ease-in-out ${props.class ?? ''}`}
+				classList={props.classList}
+			>
+				{props.children}
+			</Link>
+		</Show>
 	);
 };
 
